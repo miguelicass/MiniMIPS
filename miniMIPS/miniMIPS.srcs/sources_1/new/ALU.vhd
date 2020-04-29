af150a22007f01c9952a23e9429d0d7bd5b28e1b
Miguel Ángel <miguecasta97@gmail.com>
2020-04-29 21:30:32 +0200

    Memory created

---
+++ miniMIPS/miniMIPS.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.04.2020 20:00:25
+-- Design Name: 
+-- Module Name: ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+    Port ( clk_i : in STD_LOGIC;
+           rst_i : in STD_LOGIC;
+           en_i : in STD_LOGIC);
+end ALU;
+
+architecture Behavioral of ALU is
+
+begin
+
+
+end Behavioral;
---
+++ miniMIPS/miniMIPS.srcs/sources_1/new/RegParallel.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.04.2020 19:44:58
+-- Design Name: 
+-- Module Name: RegParallel - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RegParallel is
+    Port ( clk_i : in STD_LOGIC;
+           rst_i : in STD_LOGIC;
+           en_i : in STD_LOGIC);
+end RegParallel;
+
+architecture Behavioral of RegParallel is
+
+begin
+
+
+end Behavioral;
