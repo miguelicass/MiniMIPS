9dc9cb290f38d2123b2665d2affd0cea2a6602a6
Miguel Ángel <miguecasta97@gmail.com>
2020-05-28 23:20:10 +0200

    simulator manager1.0

--- miniMIPS/miniMIPS.srcs/sim_1/new/MIPS_tb.vhd
+++ miniMIPS/miniMIPS.srcs/sim_1/new/MIPS_tb.vhd
@@ -44,19 +44,21 @@ component MIPS is
 end component;
 
 signal clk_m, rst_m, en_m : STD_LOGIC := '0';
-signal data_m_in : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');
-signal data_m_out : STD_LOGIC_VECTOR(7 downto 0);
+signal data_m_in : STD_LOGIC_VECTOR(7 downto 0);
+signal data_m_out : STD_LOGIC_VECTOR(7 downto 0):= (others=>'0');
 
 begin
 
+    clk_m <= not clk_m after 10 ns;
+    
+    en_m <= '1'; -- para la simul
+    
     mini_mips : MIPS port map (
                     clk_m_i => clk_m,
                     rst_m_i => rst_m,
                     en_m_i => en_m,
                     data_m_i => data_m_in,
                     data_m_o => data_m_out);
-              
-    clk_m <= not clk_m after 10 ns;
-    en_m <= '1'; -- para la simul
     
+        
 end Behavioral;
---
+++ miniMIPS/miniMIPS.srcs/sim_1/new/Manager_tb.vhd
@@ -0,0 +1,73 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.05.2020 22:49:07
+-- Design Name: 
+-- Module Name: Manager_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Manager_tb is
+--  Port ( );
+end Manager_tb;
+
+architecture Behavioral of Manager_tb is
+
+component Manager is
+
+  Port (  clk_mng_i, rst_mng_i : in STD_LOGIC;
+           en_display_mng_o : out STD_LOGIC_VECTOR(7 downto 0);
+           display_mng_o : out STD_LOGIC_VECTOR(7 downto 0));
+ end component;
+
+signal clk_mng, rst_mng: STD_LOGIC := '0';
+signal en_display_mng : STD_LOGIC_VECTOR(7 downto 0):= (others=>'0');
+signal display_mng : STD_LOGIC_VECTOR(7 downto 0):= (others=>'0');
+
+
+begin
+
+    clk_mng <= not clk_mng after 10 ns;
+    
+    final_component : Manager port map (
+                        clk_mng_i => clk_mng,
+                        rst_mng_i => rst_mng,
+                        en_display_mng_o => en_display_mng,
+                        display_mng_o => display_mng);
+
+    process
+        begin
+        
+        rst_mng <= '1';
+        
+        wait for 10 ns;
+        
+        rst_mng <= '0';
+        
+    end process;
+    
+end Behavioral;
